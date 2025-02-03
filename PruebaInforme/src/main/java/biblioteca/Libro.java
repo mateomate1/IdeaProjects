0eba0ee28fc0956b67517d3e28d3f9d16193579a
mateomate1 <mateoayarrabarbero@gmail.com>
2025-02-03 13:31:51 +0100

    3/2/2025

--- PruebaInforme/src/main/java/biblioteca/Libro.java
+++ PruebaInforme/src/main/java/biblioteca/Libro.java
@@ -3,10 +3,19 @@ package biblioteca;
 public class Libro {
     private String ISBN;
     private String titulo;
+    private Autor autor;
+    private Integer ventas;
+    /*
+        public Libro(String ISBN, String titulo) {
+            this.ISBN = ISBN;
+            this.titulo = titulo;
 
-    public Libro(String ISBN, String titulo) {
+        }
+    */
+    public Libro(String ISBN, String titulo, Autor autor) {
         this.ISBN = ISBN;
         this.titulo = titulo;
+        this.autor = autor;
 
     }
 
@@ -22,5 +31,23 @@ public class Libro {
         this.titulo = titulo;
     }
 
-    public String getTitulo() { return titulo; }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Integer getVentas() {
+        return ventas;
+    }
+    public void setVentas(Integer ventas) {
+        this.ventas = ventas;
+    }
 }
+
--- PruebaInforme/src/main/java/biblioteca/Main.java
+++ PruebaInforme/src/main/java/biblioteca/Main.java
@@ -1,5 +1,6 @@
 package biblioteca;
 
+
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -13,10 +14,20 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-       Libro libro1 = new Libro("1111","biblioteca.Libro 1");
-       Libro libro2 = new Libro("2222","biblioteca.Libro 2");
-       Libro libro3 = new Libro("3333","biblioteca.Libro 3");
-       Libro libro4 = new Libro("4444","biblioteca.Libro 4");
+
+        Autor autor1 = new Autor("1Paco", "1Pill");
+        Autor autor2 = new Autor("2Paco", "2Pill");
+        Autor autor3 = new Autor("3Paco", "3Pill");
+        Autor autor4 = new Autor("4Paco", "4Pill");
+        Libro libro1 = new Libro("111111","Libro 1", autor1);
+        Libro libro2 = new Libro("222222","Libro 2", autor2);
+        Libro libro3 = new Libro("333333","Libro 3", autor3);
+        Libro libro4 = new Libro("444444","Libro 4", autor4);
+        libro1.setVentas(11);
+        libro2.setVentas(22);
+        libro3.setVentas(33);
+        libro4.setVentas(44);
+
 
         HashSet<Libro> libros = new HashSet<>();
         libros.add(libro1);
@@ -24,17 +35,17 @@ public class Main {
         libros.add(libro3);
         libros.add(libro4);
         //System.out.println("LIBROS ");
-try {
-     File fichero = new File("./informes/informeLibrosTest.jasper");
-     JasperReport informe = (JasperReport) JRLoader.loadObject(fichero);
-     JRBeanCollectionDataSource coleccion = new JRBeanCollectionDataSource(libros);
-     JasperPrint print = JasperFillManager.fillReport(informe,null,coleccion);
-    // JasperExportManager.exportReportToPdf(print,fichero);
-    JasperViewer visor = new JasperViewer(print, false);
-    visor.setVisible(true);
-    }
- catch (JRException e) {
-    e.printStackTrace();
-}
+        try {
+            File fichero = new File("./informes/Cherry.jasper");
+            JasperReport informe = (JasperReport) JRLoader.loadObject(fichero);
+            JRBeanCollectionDataSource coleccion = new JRBeanCollectionDataSource(libros);
+            JasperPrint print = JasperFillManager.fillReport(informe,null,coleccion);
+            // JasperExportManager.exportReportToPdf(print,fichero);
+            JasperViewer visor = new JasperViewer(print, false);
+            visor.setVisible(true);
+        }
+        catch (JRException e) {
+            e.printStackTrace();
+        }
     }
 }
