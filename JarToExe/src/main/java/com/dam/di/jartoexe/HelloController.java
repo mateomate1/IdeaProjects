336d5f28cc88b331757c9ed935c82b547ad03ba8
mateomate1 <mateoayarrabarbero@gmail.com>
2025-02-24 08:19:17 +0100

    24/2/2025

---
+++ JarToExe/src/main/java/com/dam/di/jartoexe/HelloController.java
@@ -0,0 +1,14 @@
+package com.dam.di.jartoexe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
